5276100f2ce40d3e1f7096d46d8a11c189a196eb
nasir <nasirmalik9641100@gmail.com>
2020-11-03 22:44:11 +0500

    Finishing UI

---
+++ lib/customIcons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  static const IconData search = IconData(0xe900, fontFamily: 'customIcons');
+  static const IconData cart = IconData(0xe901, fontFamily: 'customIcons');
+  static const IconData backbtn = IconData(0xe902, fontFamily: 'customIcons');
+  static const IconData menu = IconData(0xe903, fontFamily: 'customIcons');
+  static const IconData products = IconData(0xe904, fontFamily: 'customIcons');
+}
